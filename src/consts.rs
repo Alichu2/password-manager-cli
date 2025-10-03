eb531e392a7611a16b7db270bb3a5f848b21264f
Aliyu Nauke <aliyunauke@gmail.com>
2025-10-03 21:26:56 +0200

    Removing unused consts.

--- src/consts.rs
+++ src/consts.rs
@@ -1,10 +1,8 @@
-pub const CONF_ACCESS_CHECK: &str = "access_key_hash";
 pub const HASH_COST: u32 = 8;
 pub const LOWERCASE_CHARACTERS: &str = "abcdefghijklmnopqrstuvwxyz";
 pub const NUMBERS: &str = "0123456789";
 pub const SPECIAL_CHARACTERS: &str = "!@#$%^&*()-_=+[]{}<>/?";
 pub const BACKUP_FILE_NAME: &str = "password_backup.csv";
-pub const DATABASE_LOCATION: &str = ".password-manager/data.sqlite";
 
 pub mod communications {
     pub const WRONG_KEY: &str = "Wrong key! Try again.";
