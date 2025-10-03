f0ff251698dd4b2fa3b36800052577ae0de2fb9b
Aliyu Nauke <aliyunauke@gmail.com>
2025-10-03 18:18:08 +0200

    Updating queries.

--- src/database/queries.rs
+++ src/database/queries.rs
@@ -3,6 +3,7 @@ use sqlx::{self, SqliteConnection};
 use crate::{
     errors::Error,
     objects::query_results::{ConfigItem, ConfigParams},
+    password_operator::Password,
 };
 
 use super::manager::get_sqlite_connection;
@@ -41,14 +42,61 @@ impl DatabaseInterface {
         Ok(())
     }
 
-    pub async fn force_set_setting(&mut self, setting: ConfigItem) -> Result<(), Error> {
-        sqlx::query("INSERT INTO config (name, value) VALUES (?, ?);")
-            .bind(setting.name)
-            .bind(setting.value)
+    pub async fn create_password_table(&mut self) -> Result<(), Error> {
+        sqlx::query("CREATE TABLE passwords (password TEXT, username TEXT, place TEXT PRIMARY KEY, encrypted NUMBER);")
             .execute(&mut self.connection)
             .await
             .map_err(|err| Error::DatabaseError(err))?;
 
         Ok(())
+    }
+
+    pub async fn create_config_table(&mut self) -> Result<(), Error> {
+        sqlx::query("CREATE TABLE config (name TEXT PRIMARY KEY, value TEXT);")
+            .execute(&mut self.connection)
+            .await
+            .map_err(|err| Error::DatabaseError(err))?;
+
+        Ok(())
+    }
+
+    pub async fn load_password(&mut self, place: &str) -> Result<Vec<Password>, Error> {
+        sqlx::query_as::<_, Password>("SELECT * FROM passwords WHERE place = ?;")
+            .bind(place)
+            .fetch_all(&mut self.connection)
+            .await
+            .map_err(|err| Error::DatabaseError(err))
+    }
+
+    pub async fn delete_password(&mut self, place: &str) -> Result<(), Error> {
+        sqlx::query("DELETE FROM passwords WHERE place = ?;")
+            .bind(place)
+            .execute(&mut self.connection)
+            .await
+            .map_err(|err| Error::DatabaseError(err))?;
+
+        Ok(())
+    }
+
+    pub async fn insert_password(&mut self, password: &Password) -> Result<(), Error> {
+        sqlx::query(
+            "INSERT INTO passwords (place, password, username, encrypted) VALUES (?, ?, ?, ?);",
+        )
+        .bind(&password.place)
+        .bind(&password.password)
+        .bind(&password.username)
+        .bind(password.encrypted)
+        .execute(&mut self.connection)
+        .await
+        .map_err(|err| Error::DatabaseError(err))?;
+
+        Ok(())
+    }
+
+    pub async fn get_all_passwords(&mut self) -> Result<Vec<Password>, Error> {
+        sqlx::query_as("SELECT * FROM passwords;")
+            .fetch_all(&mut self.connection)
+            .await
+            .map_err(|err| Error::DatabaseError(err))
     }
 }
