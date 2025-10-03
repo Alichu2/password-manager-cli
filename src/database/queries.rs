e2e2f4aedd5d73769487630cf9499197e363de1c
Aliyu Nauke <aliyunauke@gmail.com>
2025-10-03 17:57:01 +0200

    Using database errors in queries.

--- src/database/queries.rs
+++ src/database/queries.rs
@@ -1,7 +1,9 @@
-use anyhow::Result;
 use sqlx::{self, SqliteConnection};
 
-use crate::objects::query_results::{ConfigItem, ConfigParams};
+use crate::{
+    errors::Error,
+    objects::query_results::{ConfigItem, ConfigParams},
+};
 
 use super::manager::get_sqlite_connection;
 
@@ -20,31 +22,32 @@ impl DatabaseInterface {
         Self { connection }
     }
 
-    pub async fn get_setting(&mut self, setting: ConfigParams) -> Result<ConfigItem> {
-        let result = sqlx::query_as::<_, ConfigItem>("SELECT * FROM config WHERE name = ?;")
+    pub async fn get_setting(&mut self, setting: ConfigParams) -> Result<ConfigItem, Error> {
+        sqlx::query_as::<_, ConfigItem>("SELECT * FROM config WHERE name = ?;")
             .bind(setting)
             .fetch_one(&mut self.connection)
-            .await?;
-
-        Ok(result)
+            .await
+            .map_err(|err| Error::DatabaseError(err))
     }
 
-    pub async fn set_setting(&mut self, setting: ConfigItem) -> Result<()> {
+    pub async fn set_setting(&mut self, setting: ConfigItem) -> Result<(), Error> {
         sqlx::query("INSERT IF NOT EXISTS INTO config (name, value) VALUES (?, ?);")
             .bind(setting.name)
             .bind(setting.value)
             .execute(&mut self.connection)
-            .await?;
+            .await
+            .map_err(|err| Error::DatabaseError(err))?;
 
         Ok(())
     }
 
-    pub async fn force_set_setting(&mut self, setting: ConfigItem) -> Result<()> {
+    pub async fn force_set_setting(&mut self, setting: ConfigItem) -> Result<(), Error> {
         sqlx::query("INSERT INTO config (name, value) VALUES (?, ?);")
             .bind(setting.name)
             .bind(setting.value)
             .execute(&mut self.connection)
-            .await?;
+            .await
+            .map_err(|err| Error::DatabaseError(err))?;
 
         Ok(())
     }
--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,9 @@
 use thiserror::Error;
 
-#[derive(Error, Debug, Clone)]
+#[derive(Error, Debug)]
 pub enum Error {
+    #[error("Error performing database action.")]
+    DatabaseError(#[from] sqlx::Error),
     #[error("Ran into an unexpected issue: {0}")]
     Other(String),
 }
